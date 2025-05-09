1326cdd7d94a8c447b451c55168af3d4f125de2b
mina <malee@DESKTOP-O27Q4BO.>
2025-05-09 19:52:43 +0900

    add mainpage

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 const express = require("express"),
   app = express(),
-  errorController = require("./controllers/errorController"),
+  router = express.Router(),
+  layouts = require("express-ejs-layouts"),
+  homeRouter = require("./routes/homepage"),
+  userRouter = require("./routes/users"),
+  accountRouter = require("./routes/accounts"),
   articleRouter = require("./routes/articles"),
-  layouts = require("express-ejs-layouts");
+  errorController = require("./controllers/errorController");
 
+// set port
 app.set("port", process.env.PORT || 3000);
+
+// set view rendering
 app.set("view engine", "ejs");
+router.use(layouts);
+router.use(express.static("public"));
 
-app.use(layouts);
-app.use(express.static("public"));
+// set router
+router.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+router.use(express.json());
 
+//app.use("/users/:userid", accountRouter);
+//app.use("/users", userRouter);
 app.use("/articles", articleRouter);
+app.use("/home", homeRouter);
 
-
-// app.use(errorController.logError);
-// app.use(errorController.respondNoResourceFound);
-// app.use(errorController.respondInternalError);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
 
 app.listen(app.get("port"), () => {
   console.log("실행 중");
---
+++ routes/accounts.js
@@ -0,0 +1,7 @@
+const express = require("express"),
+  router = express.Router(),
+  accountController = require("../controllers/accountController");
+
+//router.get("/", accountController.);
+
+module.exports = router;
