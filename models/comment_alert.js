b5d6d8b0546e6774d3812c74b1d569954a70abd7
yejin <113005678+121095@users.noreply.github.com>
2025-05-16 13:41:48 +0900

    Create comment_alert.js

---
+++ models/comment_alert.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, Sequelize) => {
+  const CommentAlert = sequelize.define('comment_alert', {
+    alert_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    alert_message: {
+      type: Sequelize.STRING(100),
+      allowNull: false
+    },
+    comment_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    user_id: { //추후 수정 필요
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true  
+    },
+    is_checked: {
+      type: Sequelize.STRING(5),
+      allowNull: false,
+      defaultValue: 'N'
+    }
+  }, {
+    timestamps: false,
+    tableName: 'comment_alert'
+  });
+
+  return CommentAlert;
+};
