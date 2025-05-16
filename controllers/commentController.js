649c8bf58931aecc98276801d7ad9e0ab2137b10
chaeseul <chaeseul123@gmail.com>
2025-05-16 17:32:30 +0900

    add delete comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,6 +1,7 @@
 const db = require("../models"),
   Comment = db.comment;
 
+// 댓글 작성
 exports.createComment = async (req, res) => {
   try {
     const { article_id, source, content } = req.body;
@@ -15,5 +16,16 @@ exports.createComment = async (req, res) => {
   } catch (err) {
     console.error("댓글 작성 중 에러:", err);
     res.status(500).send("작성 실패");
+  }
+};
+
+// 댓글 삭제
+exports.deleteComment = async (req, res) => {
+  try {
+    await Comment.destroy({ where: { comment_id: req.params.commentId } });
+    res.redirect('/articles/' + req.body.article_id);
+  } catch (err) {
+    console.error("댓글 삭제 중 에러: ", err);
+    res.status(500).send("삭제 실패");
   }
 };
--- routes/comments.js
+++ routes/comments.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
-// 댓글 작성 (POST)
+// 댓글 작성
 router.post('/', commentController.createComment);
 
+// 댓글 삭제
+router.post('/:commentId/delete', commentController.deleteComment);
 
 
 module.exports = router;
