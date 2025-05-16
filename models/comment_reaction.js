7c47c4b24cfdb3c6d1d352cf4d8b069c705daa38
yejin <113005678+121095@users.noreply.github.com>
2025-05-16 13:56:38 +0900

    Create comment_reaction.js

---
+++ models/comment_reaction.js
@@ -0,0 +1,28 @@
+
+module.exports = (sequelize, Sequelize) => {
+  const CommentReaction = sequelize.define('comment_reaction', {
+    reaction_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    reaction_type: {
+      type: Sequelize.STRING(10),
+      allowNull: false
+    },
+    comment_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    user_id: { //추가 수정 필요
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+    tableName: 'comment_reaction'
+  });
+
+  return CommentReaction;
+};
