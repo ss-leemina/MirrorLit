f212173538dd1d868302cf5757620ac2801af3d0
yejin <113005678+121095@users.noreply.github.com>
2025-05-16 15:48:07 +0900

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const commentController = require('../controllers/commentController');
 // 댓글 작성 (POST)
 router.post('/articles/:articleId/comment', commentController.createComment);
 //추천 비추천 상호작용(POST)
-router.post('/comments/:commentId/reaction', commentController.reactToComment);
+router.post('/articles/:articleId/comments/:commentId/reaction', commentController.reactToComment);
 
 
 module.exports = router;
