5733b7dcea5e0cf92e241bee443192ba9484464a
Soyeon-Son <20230871@sungshin.ac.kr>
2025-05-19 23:34:02 +0900

    Delete app.js
    app.js에 작성했던 내용은 main.js에 반영해두어서 app.js는 삭제했습니다.

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
-
-app.use(express.json());
-app.use("/users", userRoutes);
-
-module.exports = app;
