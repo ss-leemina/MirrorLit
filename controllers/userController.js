80307f835dd9e3d406056b2c40d052e3e5fc8c5e
Soyeon-Son <20230871@sungshin.ac.kr>
2025-05-26 15:11:43 +0900

    회원가입, 로그인 기능 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,46 @@
 const db = require("../models");
 const User = db.User;
+const crypto = require("crypto");
 
+// 유저 파라미터 추출
 function getUserParams(body) {
   return {
     id: body.id,
     password: body.password,
     email: body.email,
     name: body.name,
-    // 필요하다면 level_id, email_verified 등 추가 
-  };
+    level_id: 1,
+    email_verified: "N"
+    };
 }
-// 이메일 인증 상태 업데이트
+
+// 이메일로 유저 찾기
 const findUserById = async (email) => {
   return await User.findOne({ where: { email } });
 };
 
-// 회원가입 
+// 회원가입 처리
 const create = (req, res, next) => {
+  console.log("비밀번호:", req.body.password);
+  console.log("비밀번호 확인:", req.body.confirmPassword);
+
+  const { password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    req.flash("error", "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+
+    return res.render("new", {
+      name: req.body.name,
+      id: req.body.id,
+      email: req.body.email,
+      password,
+      confirmPassword,
+      messages: req.flash()
+    });
+  }
+
   const userParams = getUserParams(req.body);
+
   User.create(userParams)
     .then(user => {
       req.flash("success", `${user.name}님의 회원가입이 완료되었습니다.`);
@@ -27,21 +50,26 @@ const create = (req, res, next) => {
     })
     .catch(error => {
       console.error(`Error saving user: ${error.message}`);
-      req.flash("error", `${error.message}로 인해 회원가입에 실패했습니다.`);
-      res.locals.redirect = "/users/new";
-      next();
+      req.flash("error", error.message);
+      return res.render("new", {
+        name: req.body.name,
+        id: req.body.id,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        messages: req.flash()
+      });
     });
 };
 
-// 로그인 폼 보여주기
+// 로그인 폼
 const login = (req, res) => {
-  res.render("login");  // views/login.ejs
+  res.render("login");
 };
 
 // 로그인 인증 처리
-// Passport-local-sequelize 사용
 const authenticate = (req, res, next) => {
-  User.findOne({ where: { email: req.body.email } })
+  User.findOne({ where: { id: req.body.id } })
     .then(user => {
       if (!user) {
         req.flash("error", "존재하지 않는 계정입니다.");
@@ -72,52 +100,44 @@ const logout = (req, res, next) => {
   req.session.destroy(() => next());
 };
 
-// 리다이렉트 뷰
+// 리다이렉션 처리
 const redirectView = (req, res) => {
   res.redirect(res.locals.redirect);
 };
 
 // 이메일 인증 코드 발송
-const crypto = require("crypto");
-
 const sendResetEmail = async (req, res, next) => {
   console.log("RESET 요청 도착");
-  const { email } = req.body;
-  console.log("입력된 이메일:", email);
+  const { email, name, id, password, confirmPassword } = req.body;
 
+  res.locals.email = email;
+  res.locals.name = name;
+  res.locals.id = id;
+  res.locals.password = password;
+  res.locals.confirmPassword = confirmPassword;
 
   try {
     const user = await User.findOne({ where: { email } });
-    console.log("유저 검색 결과:", user ? user.email : "유저 없음");
-
 
     if (!user) {
-      console.log("유저 없음 - redirect");
-      req.flash("error", "존재하지 않는 이메일입니다.");
-      res.locals.redirect = "/users/reset-password";
+      console.log("유저 없음 - 인증코드 생성만 수행");
+
+      const code = crypto.randomInt(100000, 999999).toString();
+      console.log("생성된 인증코드:", code);
+
+      req.flash("success", `입력한 이메일로 콘솔에 인증코드를 보냈습니다.`);
+      res.locals.redirect = "/users/new";
+      return next();
+    } else {
+      req.flash("error", "이미 가입된 이메일입니다.");
+      res.locals.redirect = "/users/new";
       return next();
     }
-
-    // 인증코드 생성 (6자리 난수)
-    const code = crypto.randomInt(100000, 999999).toString();
-    user.authCode = code;
-    await user.save();
-
-    console.log("생성된 인증코드:", code);
-
-    // 실제 메일 보내는 부분은 nodemailer로 추후 구현
-    //console.log(`인증코드: ${code} (임시 출력)`);
-
-     res.locals.email = email;
-     req.flash("success", `${user.name}님에게 인증코드를 발송했습니다.`);
-     res.locals.user = user;
-     res.locals.redirect = "/users/reset-password";
-     next();
   } catch (error) {
     console.error("인증코드 발송 오류:", error.message);
     req.flash("error", "인증코드 발송 중 오류 발생");
-    res.locals.redirect = "/users/reset-password";
-    next();
+    res.locals.redirect = "/users/new";
+    return next();
   }
 };
 
@@ -134,10 +154,9 @@ const verifyAuthCode = async (req, res, next) => {
       return next();
     }
 
-    // 인증 성공
     req.flash("success", "인증 성공! 비밀번호를 재설정하세요.");
     res.locals.user = user;
-    res.locals.redirect = "/users/reset-form"; // resetPassword2.ejs로 이동
+    res.locals.redirect = "/users/reset-form";
     next();
   } catch (err) {
     console.error(err.message);
@@ -147,16 +166,15 @@ const verifyAuthCode = async (req, res, next) => {
   }
 };
 
-
-
-// 비밀번호 재설정 폼 보여주기
+// 비밀번호 재설정 폼
 const showResetForm = (req, res) => {
-  res.render("resetPassword2");  // views/resetPassword2.ejs
+  res.render("resetPassword2");
 };
 
 // 최종 비밀번호 변경 처리
 const resetPasswordFinal = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+
   if (password !== confirmPassword) {
     req.flash("error", "비밀번호와 확인이 일치하지 않습니다.");
     res.locals.redirect = "/users/reset-form";
@@ -204,12 +222,19 @@ const verifyEmail = (req, res, next) => {
 
 // 회원가입 폼 렌더링
 const showSignupForm = (req, res) => {
-  res.render("new");            // views/new.ejs
+  res.render("new", {
+    email: res.locals.email || "",
+    name: res.locals.name || "",
+    id: res.locals.id || "",
+    password: res.locals.password || "",
+    confirmPassword: res.locals.confirmPassword || "",
+    messages: req.flash()
+  });
 };
 
 // 인증코드 요청 폼 렌더링
 const showResetRequestForm = (req, res) => {
-  res.render("resetPassword");  // views/resetPassword.ejs
+  res.render("resetPassword");
 };
 
 module.exports = {
