048e5f2f653759338ac9d104ac9123a54f451bec
0-ch-sl-0 <20221170@sungshin.ac.kr>
2025-05-27 18:03:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -48,8 +48,7 @@ app.use((req, res, next) => {
 app.use(passport.initialize());
 app.use(passport.session());
 
-db.sequelize.sync({ alter: true });
-
+db.sequelize.sync();
 // db.sequelize.sync({ alter: true });  // sequelize 바꾸면 이걸로 바꿔서 동기화
 
 // set local data
