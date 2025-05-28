2f26d9da70788711ee512d0aa7cd1a5bce53bddd
Soyeon-Son <20230871@sungshin.ac.kr>
2025-05-28 22:26:04 +0900

    로그인, 회원가입 최종 수정 완료

--- models/userModel.js
+++ models/userModel.js
@@ -16,16 +16,13 @@ module.exports =  (sequelize, Sequelize) => {
     //등급 저장
     rank_id: {
       type: Sequelize.INTEGER,
-      reference: {
+      references: {
         model: 'user_rank',
         key: 'rank_id'
       },
       defaultValue: 1 //신규: 1 기존: 2
     },
-    password: {
-      type: Sequelize.STRING(100), //bcrypt 해시 길이 때문에 100으로 설정
-      allowNull: false
-    },
+    
     email: {
       type: Sequelize.STRING(50),
       unique: true,
@@ -41,11 +38,7 @@ module.exports =  (sequelize, Sequelize) => {
       defaultValue: Sequelize.NOW,
       allowNull: true
     },
-    level_id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
+    
     email_verified: {
       type: Sequelize.ENUM("Y", "N"),
       allowNull: false,
@@ -53,8 +46,14 @@ module.exports =  (sequelize, Sequelize) => {
     },
       
     // passport-local-sequelize 용 해시/솔트 필드
-    myhash: Sequelize.STRING,
-    mysalt: Sequelize.STRING
+    myhash: {
+  type: Sequelize.STRING(1024), 
+  allowNull: false
+},
+mysalt: {
+  type: Sequelize.STRING(255), 
+  allowNull: false
+}
   },
     {
       timestamps: false,
@@ -67,43 +66,23 @@ module.exports =  (sequelize, Sequelize) => {
     saltField: "mysalt"
   });
 
-  const createUser = async (id, password, name, email) => {
-    const query = `
-      INSERT INTO users (id, password, name, email)
-      VALUES (?, ?, ?, ?)
-    `;
-    await db.execute(query, [id, password, name, email]);
-  };
+  
 
-  User.beforeCreate(async (user, options) => {
-    const bcrypt = require("bcrypt");
-    const saltRounds = 10;
-    user.password = await bcrypt.hash(user.password, saltRounds);
-  });
+  //User.beforeCreate(async (user, options) => {
+    //const bcrypt = require("bcrypt");
+    //const saltRounds = 10;
+    //user.password = await bcrypt.hash(user.password, saltRounds);
+  //});
 
-  User.beforeUpdate(async (user, options) => {
-    if (user.changed('password')) {
-      const bcrypt = require("bcrypt");
-      const saltRounds = 10;
-      user.password = await bcrypt.hash(user.password, saltRounds);
-    }
-  });
+  //User.beforeUpdate(async (user, options) => {
+    //if (user.changed('password')) {
+      //const bcrypt = require("bcrypt");
+      //const saltRounds = 10;
+      //user.password = await bcrypt.hash(user.password, saltRounds);
+    //}
+  //});
 
-  const findUserById = async (id) => {
-    const query = `SELECT * FROM users WHERE id = ?`;
-    const [rows] = await db.execute(query, [id]);
-    return rows[0];
-  };
-
-  const updatePassword = async (userId, newPassword) => {
-    const query = `UPDATE users SET password = ? WHERE id = ?`;
-    await db.execute(query, [newPassword, userId]);
-  };
-
-  const updateEmailVerifiedStatus = async (userId, status) => {
-    const query = `UPDATE users SET email_verified = ? WHERE user_id = ?`;
-    await db.execute(query, [status, userId]);
-  };
+  
 
   return User;
 }
