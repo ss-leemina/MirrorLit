737f9d04c7b61e81af53d9585dd25ee6bd055752
hyeonsyeon <20201012@sungshin.ac.kr>
2025-05-31 01:46:06 +0900

    DB에 넣은 부분 삭제

--- models/user_rank.js
+++ models/user_rank.js
@@ -1,6 +1,3 @@
-// 이거 db에 넣었습니다! 지워도 괜찮은거면 지워주세요~
-// INSERT INTO user_rank(rank_name, min_comments, min_upvotes) VALUES ('신규', 0, 0), ('기존', 5, 10); 
-
 module.exports = (sequelize, Sequelize) => {
 	const UserRank = sequelize.define('user_rank', {
 		rank_id: {
