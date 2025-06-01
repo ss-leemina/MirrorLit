0e1d16e61c36bdf90faea73fbd0030a013e403ba
mina <malee@DESKTOP-O27Q4BO.>
2025-06-02 03:37:38 +0900

    add user notification

--- controllers/alertController.js
+++ controllers/alertController.js
@@ -1,6 +1,7 @@
 const db = require("../models"),
   CommentAlert = db.CommentAlert,
-  Comment = db.comment;
+  Comment = db.comment,
+  Notification = db.UserNotification;
 
 // 알림 목록 조회
 exports.getAlertsByUser = async (req, res) => {
@@ -19,7 +20,7 @@ exports.getAlertsByUser = async (req, res) => {
   }
 };
 
-// 알림 읽음 처리
+// 댓글 알림 읽음 처리
 exports.markAlertAsRead = async (req, res) => {
   try {
     const alertId = req.params.alert_id;
@@ -36,7 +37,7 @@ exports.markAlertAsRead = async (req, res) => {
         return res.status(404).json({ message: "해당 알림을 찾을 수 없습니다." });
       }
     }
-    console.log("알림을 읽음으로 표시했습니다.");
+    console.log("댓글 알림을 읽음으로 표시했습니다.");
 
     // 알림이 온 기사로 redirection
     const commentId = Alert.getDataValue('comment_id');
@@ -50,18 +51,44 @@ exports.markAlertAsRead = async (req, res) => {
   }
 };
 
+// 사용자 알림 읽음 처리
+exports.markNotificationAsRead = async (req, res) => {
+  try {
+    const alertId = req.params.notification_id;
+    const Alert = await Notification.findOne({ where: { notification_id: alertId} });
+
+    // 알림이 읽지 않음 상태일 때만 읽음 처리
+    if(Alert.getDataValue('is_checked') === 'N') {
+      const updated = await Notification.update(
+        { is_checked: 'Y' },
+        { where: { notification_id: alertId } }
+      );
+
+      if (updated[0] === 0) {
+        return res.status(404).json({ message: "해당 알림을 찾을 수 없습니다." });
+      }
+    }
+    console.log("사용자 알림을 읽음으로 표시했습니다.");
+
+    // 마이페이지로 redirection
+    return res.redirect(`/rank/users/${res.locals.currentUser.user_id}`);
+  } catch (err) {
+    console.error("알림 읽음 처리 실패:", err);
+    res.status(500).json({ message: "알림 상태 변경 실패" });
+  }
+};
+
 // 모든 알림 읽음 처리
 exports.markAlertAsReadAll = async (req, res) => {
   try {
-    // 사용자의 모든 알림 확인
+    // 사용자의 모든 댓글 알림 확인
     const userId = req.params.user_id;
 
     const alerts = await CommentAlert.findAll({
-      where: { user_id: userId },
-      order: [['alert_id', 'DESC']]
+      where: { user_id: userId }
     });
 
-    // 각 알림을 읽음으로 표시
+    // 각 댓글 알림을 읽음으로 표시
     await alerts.forEach(alr => {
       if(alr.getDataValue('is_checked') === 'N')
         CommentAlert.update(
@@ -69,7 +96,23 @@ exports.markAlertAsReadAll = async (req, res) => {
           { where: { alert_id: alr.alert_id } }
         );
     });
-    console.log("알림을 읽음으로 표시했습니다.");
+    console.log("모든 댓글 알림을 읽음으로 표시했습니다.");
+
+
+    // 사용자의 모든 사용자 알림 확인
+    const notifications = await Notification.findAll({
+      where: { user_id: userId }
+    });
+
+    // 각 댓글 알림을 읽음으로 표시
+    await notifications.forEach(ntf => {
+      if(ntf.getDataValue('is_checked') === 'N')
+        Notification.update(
+          { is_checked: 'Y'},
+          { where: { notification_id: ntf.notification_id } }
+        );
+    });
+    console.log("모든 사용자 알림을 읽음으로 표시했습니다.");
 
     // 기존 페이지 또는 홈페이지로 redirection
     return res.redirect(req.headers.referer || "/home");
@@ -79,12 +122,27 @@ exports.markAlertAsReadAll = async (req, res) => {
   }
 };
 
-// 알림 삭제
+// 댓글 알림 삭제
 exports.deleteAlert = async (req, res) => {
   try {
     const alertId = req.params.alert_id;
     await CommentAlert.destroy({ where: { alert_id: alertId } });
-    console.log("알림을 삭제했습니다.");
+    console.log("댓글 알림을 삭제했습니다.");
+
+    // 기존 페이지 또는 홈페이지로 redirection
+    return res.redirect(req.headers.referer || "/home");
+  } catch (err) {
+    console.error("알림 삭제 실패:", err);
+    res.status(500).json({ message: "알림 삭제 실패" });
+  }
+};
+
+// 사용자 알림 삭제
+exports.deleteNotification = async (req, res) => {
+  try {
+    const alertId = req.params.notification_id;
+    await Notification.destroy({ where: { notification_id: alertId } });
+    console.log("사용자 알림을 삭제했습니다.");
 
     // 기존 페이지 또는 홈페이지로 redirection
     return res.redirect(req.headers.referer || "/home");
@@ -97,19 +155,29 @@ exports.deleteAlert = async (req, res) => {
 // 모든 알림 삭제
 exports.deleteAlertAll = async (req, res) => {
   try {
-    // 사용자의 모든 알림 확인
+    // 사용자의 모든 댓글 알림 확인
     const userId = req.params.user_id;
 
     const alerts = await CommentAlert.findAll({
-      where: { user_id: userId },
-      order: [['alert_id', 'DESC']]
+      where: { user_id: userId }
     });
 
-    // 각 알림 삭제
+    // 각 댓글 알림 삭제
     await alerts.forEach(alr => {
       CommentAlert.destroy({ where: { alert_id: alr.alert_id } });
     });
-    console.log("모든 알림을 삭제했습니다.");
+    console.log("모든 댓글 알림을 삭제했습니다.");
+
+    // 사용자의 모든 사용자 알림 확인
+    const notifications = await Notification.findAll({
+      where: { user_id: userId }
+    });
+
+    // 각 사용자 알림 삭제
+    await notifications.forEach(ntf => {
+      Notification.destroy({ where: { notification_id: ntf.notification_id } });
+    });
+    console.log("모든 사용자 알림을 삭제했습니다.");
 
     // 기존 페이지 또는 홈페이지로 redirection
     return res.redirect(req.headers.referer || "/home");
--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -6,13 +6,15 @@ const alertController = require('../controllers/alertController');
 router.get('/:user_id', alertController.getAlertsByUser);
 
 //알림 읽음 처리
-router.post('/:alert_id/read', alertController.markAlertAsRead);
+router.post('/alert/:alert_id/read', alertController.markAlertAsRead);
+router.post('/notification/:notification_id/read', alertController.markNotificationAsRead);
 
 // 모든 알림 읽음 처리
 router.post('/:user_id/readAll', alertController.markAlertAsReadAll);
 
 // 알림 삭제
-router.post('/:alert_id/delete', alertController.deleteAlert);
+router.post('/alert/:alert_id/delete', alertController.deleteAlert);
+router.post('/notification/:notification_id/delete', alertController.deleteNotification);
 
 // 모든 알림 삭제
 router.post('/:user_id/deleteAll', alertController.deleteAlertAll);
